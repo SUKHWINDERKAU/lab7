b89f2538461e2cc91d4eb2d572665819eefc28be
SUKHWINDERKAU <156840502+SUKHWINDERKAU@users.noreply.github.com>
2024-02-14 16:26:14 -0500

    Add files via upload

---
+++ footer-nav.php
@@ -0,0 +1,21 @@
+<!--cut from footer-nav.php -->
+<!-- creating footer-nav.php  AND PASTING THE FOLLOWING CODE-->
+<!-- Provide links to privacy policy and terms of use -->
+
+
+<nav>
+    <ul>
+         <li><a href="/week-7-complete/about/privacy-policy.php" title="Read our Privacy Policy">Privacy Policy</a></li>
+         <li>
+             <a href="/week-7-complete/about/privacy-policy.php#personal-information" title="Learn about how we treat your personal information">Use of Personal Information</a> 
+        </li>
+        <li>
+             <a href="/week-7-complete/about/terms-of-use.php" title="Understand our Terms of Use">Terms of Use</a>
+         </li>
+         <li>
+             <a href="/week-7-complete/about/terms-of-use.php#limitation-liability" title="Our Limitation of Liability explained">Limitation of Liability</a>
+         </li>
+
+					
+    </ul>
+</nav>
